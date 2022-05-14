a9dffdf50a5199c693cfcb8083b8f99fb7a2c387
ryonistic@protonmail.com <ryonistic@protonmail.com>
2022-05-14 16:26:08 +0530

    admission form added

--- config/settings.py
+++ config/settings.py
@@ -180,6 +180,8 @@ BLEACH_STRIP_COMMENTS = False
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During development only
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+STRIPE_PUBLIC_KEY = "pk_test_1234"
+STRIPE_SECRET_KEY = "sk_test_1234"
 
 SITE_ID = 1
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
